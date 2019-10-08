bde73ccb37b3d051a18cfc2637cdf2183a37efb6
Aidan Robinson <aidanrobinson6@gmail.com>
2019-10-09 11:48:29 +1300

    Aerial_MovementV2

---
+++ New Unity Project/Assets/_Scripts/Player_Death.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class Player_Death : MonoBehaviour
+{
+
+    private void OnTriggerEnter(Collider other)
+    {
+        SceneManager.LoadScene(0);
+
+    }
+}
